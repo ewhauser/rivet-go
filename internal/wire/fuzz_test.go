c5fba6d4c2a144e3a3b3f5fec00e5c9d1451152e
Eric Hauser <ewhauser@gmail.com>
2026-08-03 16:42:00 -0600

    test: seed fuzzer with M5 alarm golden
    60s run, no findings.

--- internal/wire/fuzz_test.go
+++ internal/wire/fuzz_test.go
@@ -24,6 +24,7 @@ func FuzzDecodeEventBatch(f *testing.F) {
 		"event_ws_open.msgpack",
 		"event_ws_message.msgpack",
 		"event_ws_close.msgpack",
+		"event_actor_alarm.msgpack",
 	} {
 		data, err := os.ReadFile(filepath.Join("testdata", name))
 		if err != nil {
