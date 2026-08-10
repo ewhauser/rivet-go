9fa42bf2c4837d45f5c9a1a8fd159fd1269b6fe8
Eric Hauser <ewhauser@gmail.com>
2026-08-10 04:29:03 -0600

    Broadcast engine process completion

--- internal/devengine/devengine_test.go
+++ internal/devengine/devengine_test.go
@@ -2,8 +2,10 @@ package devengine
 
 import (
 	"context"
+	"os/exec"
 	"path/filepath"
 	"testing"
+	"time"
 )
 
 func TestPinnedBuildRunsFromSourceCheckout(t *testing.T) {
@@ -23,5 +25,36 @@ func TestPinnedBuildRunsFromSourceCheckout(t *testing.T) {
 	}
 	if !foundManifest {
 		t.Fatalf("cargo arguments %q do not select manifest %q", command.Args, wantManifest)
+	}
+}
+
+func TestEngineProcessExitCanBeObservedByConcurrentKillCallers(t *testing.T) {
+	process := &engineProcess{
+		command: &exec.Cmd{},
+		done:    make(chan struct{}),
+	}
+	close(process.done)
+
+	engine := &Engine{process: process}
+	const callers = 10
+	start := make(chan struct{})
+	results := make(chan error, callers)
+	for range callers {
+		go func() {
+			<-start
+			results <- engine.killProcess(process)
+		}()
+	}
+	close(start)
+
+	for range callers {
+		select {
+		case err := <-results:
+			if err != nil {
+				t.Fatalf("observe completed process: %v", err)
+			}
+		case <-time.After(time.Second):
+			t.Fatal("concurrent kill caller did not observe process completion")
+		}
 	}
 }
